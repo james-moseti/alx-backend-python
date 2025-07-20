af0a1dd606284389b71296142ccd2e0af28f22e4
James Moseti <moseti.james4@gmail.com>
2025-07-20 22:58:50 +0300

    fix: use routers.DefaultRouter()

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
+from rest_framework import routers
 from .views import (
     ConversationViewSet,
     MessageViewSet,
@@ -7,8 +7,7 @@ from .views import (
     UserViewSet
 )
 
-# Create a router and register our viewsets with it
-router = DefaultRouter()
+router = routers.DefaultRouter()
 router.register(r'conversations', ConversationViewSet, basename='conversation')
 router.register(r'messages', MessageViewSet, basename='message')
 router.register(r'bulk', ConversationBulkViewSet, basename='conversation-bulk')
@@ -17,6 +16,5 @@ router.register(r'users', UserViewSet, basename='user')
 app_name = 'messaging'
 
 urlpatterns = [
-    # Include all router URLs
     path('', include(router.urls)),
 ]
